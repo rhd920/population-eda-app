7b6c70ba00c34d064a899a4564e9de01217094e9
rhd920 <ts24049@ts.hs.kr>
2025-06-17 06:04:21 +0000

    2차 수정

--- app_eda.py
+++ app_eda.py
@@ -419,10 +419,9 @@ class EDA:
             display_df = top_changes[['연도', '지역', '인구', '증감']].reset_index(drop=True)
 
             # 스타일링 및 출력
-            styled_df = display_df.style.applymap(highlight_change, subset=['증감']) \
-                                        .format({'인구': '{:,}', '증감': '{:,}'})
+            styled_df = display_df.style.applymap(highlight_change, subset=['증감']).format({'인구': '{:,}', '증감': '{:,}'})
 
-            st.dataframe(styled_df, use_container_width=True)
+            st.write(styled_df)
 
             st.markdown("""
                 - **Top 100 population changes** across regions over all years.
